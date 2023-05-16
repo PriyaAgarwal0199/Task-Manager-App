ad61b521e73e909ebd8f947ee6e0842fb27589d4
Priya Agarwal <agarwalpriya0199@gmail.com>
2023-05-16 17:26:41 +0530

    package-lock removal

--- src/emails/account.js
+++ src/emails/account.js
@@ -8,7 +8,7 @@ const sendEmail = async (to,name,subject,context)=>{
     if(context === "welcome")
     text = `Welcome to the app, ${name}. Let me know how you get along with the app.`
     else if(context==="remove account")
-    text = `It's sad to see you go ${name}, can you please tell us what made you come to this decision and how can we further improve? I hope to see you back sometime soon.`
+    text = `It's sad to see you go ${name}, can you please tell us what made you come to this decision and how can we further improve? Hope to see you back sometime soon.`
     await sgMail.send({
         to: to,
         from:'peehuagarwal0199@gmail.com',
